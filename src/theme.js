9fbe4ab3958e00028d638846e094c062a17eeb83
wakfuld23 <63659885+wakfuld23@users.noreply.github.com>
2021-07-23 17:49:44 +0200

    add: theme

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+
+const theme = {
+    fonts: {
+        primary: `'Spartan', sans-serif`,
+        secondary: `'Antonio', sans-serif`,
+    },
+    colors: {
+        white: 'hsl(0, 0%, 100%)',
+        whiteAlpha50: 'hsla(0, 0%, 100%, 0.5)',
+        whiteAlpha75: 'hsla(0, 0%, 100%, 0.75)',
+        black: 'hsl(240, 67%, 8%)',
+        darkGray: 'hsl(240, 17%, 26%)',
+        lightGray: 'hsl(240, 6%, 54%)',
+        turquoise: 'hsl(194, 48%, 49%)',
+        lightOrange: 'hsl(33, 82%, 61%)',
+        darkOrange: 'hsl(10, 63%, 51%)',
+        purple: 'hsl(263, 67%, 51%)',
+        lightRed: 'hsl(2, 68%, 53%)',
+        darkRed: 'hsl(17, 73%, 46%)',
+        green: 'hsl(169, 73%, 44%)',
+        blue: 'hsl(222, 87%, 56%)',
+    },
+    
+};
+
+export default theme;
